0fbbf5a3d1b9f368409aaf8a56add13e2c7f845a
Simon Moore <simon.moore@cl.cam.ac.uk>
2022-08-25 09:10:23 +0100

    Exit on first error

--- software/app/swm_rebuild.sh
+++ software/app/swm_rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-LM_LICENSE_FILE=;time quartus_pgm -m jtag -o "p;../../output_files/DE10_Pro.sof@1"
-pushd ../;./swm_generate_bsp.sh;popd
-make clean_all
-make
+LM_LICENSE_FILE=;time quartus_pgm -m jtag -o "p;../../output_files/DE10_Pro.sof@1" || exit 1
+pushd ../
+./swm_generate_bsp.sh || exit 1
+popd
+make clean_all || exit 1
+make || exit 1
