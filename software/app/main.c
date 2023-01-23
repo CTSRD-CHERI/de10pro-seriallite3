a73983d7fe4a6309ef254baf50115bd12e2eec06
Simon Moore <simon.moore@cl.cam.ac.uk>
2023-01-23 15:24:39 +0000

    Add more tests

--- software/app/main.c
+++ software/app/main.c
@@ -1,9 +1,9 @@
+#include <stdbool.h>
 #include "HAL/inc/sys/alt_stdio.h"
+//#include <stdio.h>
+#include <fcntl.h>
 #include "HAL/inc/io.h"
 #include "system.h"
-//#include "altera_avalon_fifo.h"
-//#include "altera_avalon_fifo_util.h"
-//#include "altera_avalon_fifo_regs.h"
 #include "unistd.h"
 
 /*****************************************************************************
@@ -12,6 +12,9 @@
  *   - rx_fifo_rd_en
  *   - rx_fifo_align_clr
  *****************************************************************************/
+
+const int word_offset=8;
+
 
 struct fifoDetails {
   alt_u32 base_addr;
@@ -34,8 +37,9 @@ print_n_tabs(int n)
 }
 
 
+
 void
-print_link_status()
+print_link_status_old()
 {
   int base = PIO_STATUS_BASE;
   int status;
@@ -76,7 +80,7 @@ check_testreg(struct fifoDetails f)
 {
   int j,t,d;
   int pass=1;
-  int testreg_addr_offset=0x10*8;
+  int testreg_addr_offset=0x10*word_offset;
   for(j=0; j<10; j++) {
     t=j;
     IOWR_32DIRECT(f.base_addr, testreg_addr_offset, ~t);
@@ -94,7 +98,7 @@ int
 status_fifo(struct fifoDetails f, int fifonum)
 {
   int status;
-  status = IORD_32DIRECT(f.base_addr,8*(1+2*fifonum));
+  status = IORD_32DIRECT(f.base_addr,word_offset*(1+2*fifonum));
   //  alt_printf("DEBUG: Chan %c: status=0x%x\n", f.chan_letter, status);
   return status;
 }
@@ -118,21 +122,32 @@ void
 write_tx_fifo(struct fifoDetails f, int fifonum, int data)
 {
   // TODO: check FIFO isn't full!!!
-  IOWR_32DIRECT(f.base_addr, 8*2*fifonum, data);
+  IOWR_32DIRECT(f.base_addr, word_offset*2*fifonum, data);
+}
+
+
+int
+read_rx_fifo(struct fifoDetails f, int fifonum, int* data)
+{
+  if(status_rx_fifo_notEmpty(f, fifonum)) {
+    (*data) = IORD_32DIRECT(f.base_addr,word_offset*2*fifonum);
+    return true;
+  } else {
+    (*data) = 0;
+    return false;
+  }
 }
 
 
 void
-read_rx_fifo(struct fifoDetails f, int fifonum, int chan_index, int silent)
+report_rx_fifo(struct fifoDetails f, int fifonum, int chan_index, int silent)
 {
-  int data, ctr;
-  ctr = 0;
-  while(status_rx_fifo_notEmpty(f, fifonum)) {
-    data = IORD_32DIRECT(f.base_addr,8*2*fifonum);
-    print_n_tabs(chan_index*3);
-    if(silent==0)
-      alt_printf("0x%x: RX-%c 0x%x\n",ctr,f.chan_letter,data);
-    ctr++;
+  int data;
+  while(read_rx_fifo(f, fifonum, &data)) {
+    if(silent==false) {
+      print_n_tabs(chan_index*3);
+      alt_printf("RX-%c 0x%x\n",f.chan_letter,data);
+    }
   }
 }
 
@@ -147,24 +162,68 @@ status_device(int csr_index)
 int chip_id_lo() { return status_device(4); }
 int chip_id_hi() { return status_device(5); }
 
+void
+print_link_status(struct fifoDetails f, int fifonum)
+{
+  int status;
+  // assert 0<=fifonum<=3
+  status = status_device(fifonum);
+  alt_printf("Chan %c: Link up: tx=%x,rx=%x;  error_tx=0x%x,  error_rx=0x%x,  calibration_busy (binary)=%x%x,  htile_lock (binary)=%x%x\n",
+	     f.chan_letter,
+	     exbit(status,0),
+	     exbit(status,1),
+	     (status>>2) & 0xf,
+	     (status>>6) & 0x1f,
+	     exbit(status,18),
+	     exbit(status,19),
+	     exbit(status,16),
+	     exbit(status,17));
+}
+
+
+
+void
+test_write_read_channels(struct fifoDetails* fs, int num_chan)
+{
+  int j, chan;
+  int cid0 = chip_id_lo();
+  alt_printf("Write-read tests on the channels\n");
+  for(j=0; j<100; j++) {
+    for(chan=0; chan<num_chan; chan++)
+      write_tx_fifo(fs[chan], 0, (cid0<<16) | (j+1));
+    for(chan=0; chan<num_chan; chan++) {
+      report_rx_fifo(fs[chan], 0, chan, false);
+    }
+    usleep(500000);
+  }
+}
+
+
 
 int
 main(void)
 {
-  const int num_chan = 2;
+  const int num_chan = 4;
   const int fifonum = 0; // 0=serial-link, 1=loopback
   struct fifoDetails fs[num_chan];
   //int phy_mgmt_addr_offset = 1<<15; // word address offset to access physical management (PMA) addresses; MSB of address bits
   //int j, d, e, status;
   int j, chan;
+  char c;
+  int flush_mode = true;
 
   alt_putstr("Start...\n");
+
   alt_printf("ChipID = 0x%x %x\n", chip_id_hi(), chip_id_lo());
   // Check testreg to ensure we're probably communicating with a BERT
   fs[0].base_addr = MKBERT_INSTANCE_0_BASE;
   fs[0].chan_letter = 'A';
   fs[1].base_addr = MKBERT_INSTANCE_1_BASE;
-  fs[1].chan_letter = 'D';
+  fs[1].chan_letter = 'B';
+  fs[2].base_addr = MKBERT_INSTANCE_2_BASE;
+  fs[2].chan_letter = 'C';
+  fs[3].base_addr = MKBERT_INSTANCE_3_BASE;
+  fs[3].chan_letter = 'D';
   for(j=0; j<num_chan; j++) {
     alt_printf("BERT 0x%x on Chan %c at base address 0x%x\n",
 	       j,
@@ -173,31 +232,69 @@ main(void)
     check_testreg(fs[j]);
   }
 
-  print_link_status();
+  for(chan=0; chan<num_chan; chan++)
+    print_link_status(fs[chan], chan);
 
-  alt_printf("Write-read tests on the channels\n");
-  for(j=0; j<10; j++)
-      for(chan=0; chan<num_chan; chan++) {
-	write_tx_fifo(fs[chan], fifonum, (1<<16) | (j+1));
-	read_rx_fifo(fs[chan], fifonum, 0, 0);
-      }
+  // test_write_read_channels(fs, num_chan);
   
-  for(j=0; j<5; j++)
+  // set stdin to nonblocking to allow keyboard polling
+  fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
+
+  alt_putstr("Start tests:\n");
+  alt_putstr("   f = flush then exit\n");
+  alt_putstr("   l = loop-back\n");
+  alt_putstr("   t = test\n");
+  c=' ';
+  while (flush_mode) {
+    c = alt_getchar();
+    if((int) c > 0) {
+      if(c=='\004') // exit on ctl-D
+	return 0;
+      alt_printf("c = 0x%x = %c\n", c, c);
+      if((c=='l') || (c=='t') || (c=='f')) flush_mode = false;
       for(chan=0; chan<num_chan; chan++)
-	read_rx_fifo(fs[chan], fifonum, 0, 0);
+	report_rx_fifo(fs[chan], fifonum, chan, true);
+    }
+  }
 
-  chan=0;
-  for(j=0; j<100; j++)
-    if(status_tx_fifo_notFull(fs[chan], fifonum))
-      write_tx_fifo(fs[chan], fifonum, (1<<16) | (j+1));
-    else
-      alt_printf("Managed to fill up the tx fifo.  j=0x%x\n",j);
+  if(c=='l') {
+    alt_putstr("Loopback mode\n");
+    int cid0 = chip_id_lo();
+    while(1) {
+      for(chan=0; chan<num_chan; chan++) {
+	int data;
+	if(read_rx_fifo(fs[chan], fifonum, &data)) {
+	  alt_printf("Loopback chan %c = 0x%x\n", fs[chan].chan_letter, data);
+	  write_tx_fifo(fs[chan], fifonum, (cid0<<16) | (data & 0xffff) );
+	}
+      }
+    }
+  }
 
-  alt_printf("Reading all data from the FIFOs\n");
-  for(j=0; j<200; j++)
-    for(chan=0; chan<num_chan; chan++)
-      read_rx_fifo(fs[chan], fifonum, 0, 0);
+  if(c=='f') {
+    alt_printf("Flushing data left in RX FIFOs\n");
+    for(j=0; j<200; j++)
+      for(chan=0; chan<num_chan; chan++)
+	report_rx_fifo(fs[chan], fifonum, chan, false);
+  }
 
+  if(c=='t') {
+    alt_printf("Write-read tests on the channels\n");
+    for(j=0; j<10; j++) {
+      for(chan=0; chan<num_chan; chan++)
+	write_tx_fifo(fs[chan], fifonum, ((chan|0x1000)<<16) | (j+1));
+      //      write_tx_fifo(fs[chan], fifonum, (cid0<<16) | (j+1));
+      for(chan=0; chan<num_chan; chan++) {
+	alt_printf("Chan=%c\n", fs[chan].chan_letter);
+	report_rx_fifo(fs[chan], fifonum, chan, false);
+      }
+    }
+  
+    for(j=0; j<100; j++)
+      for(chan=0; chan<num_chan; chan++)
+	report_rx_fifo(fs[chan], fifonum, 0, false);
+  }
+  
   alt_putstr("The end\n\n");
   usleep(1000000);
 
