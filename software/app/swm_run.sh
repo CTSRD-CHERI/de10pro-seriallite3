affe73135041e0fe197295ef1715381d9dedd097
Simon Moore <simon.moore@cl.cam.ac.uk>
2023-01-23 10:02:39 +0000

    Accept a cable number as a command-line argument.

--- software/app/swm_run.sh
+++ software/app/swm_run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 cable_number"
+    exit 1
+fi
+
 make || exit 1
-nios2-download -r -g main.elf || exit 1
-nios2-terminal || exit 1
+
+cable=$1
+nios2-download --cable $cable -r -g main.elf || exit 1
+nios2-terminal --cable $cable || exit 1
 
