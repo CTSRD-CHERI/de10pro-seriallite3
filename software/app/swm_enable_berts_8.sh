dde9a975406f86c7c2efd9583f0de8fec2b35315
Simon Moore <simon.moore@cl.cam.ac.uk>
2023-02-01 19:40:52 +0000

    BERT control scripts.

---
+++ software/app/swm_enable_berts_8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+date
+pids=""
+for cable in {1..8}
+do
+    echo "JTAG chain number: " $cable > swm_enable_berts_log.$cable
+    ( echo '1' | ./swm_run.sh $cable | grep 'BERT enable'  >> swm_enable_berts_log.$cable ) &
+    pids="$pids $!"
+done
+wait $pids
+for cable in {1..8}
+do
+    cat swm_enable_berts_log.$cable
+    rm swm_enable_berts_log.$cable
+done
+
