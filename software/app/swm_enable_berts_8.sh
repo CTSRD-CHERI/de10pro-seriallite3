a87bad4d5dc7a17156a802250c53c75830439391
Simon Moore <simon.moore@cl.cam.ac.uk>
2023-02-01 19:44:49 +0000

    Refinements

--- software/app/swm_enable_berts_8.sh
+++ software/app/swm_enable_berts_8.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+make || exit 1
 date
 pids=""
 for cable in {1..8}
 do
+    nios2-download --cable $cable -r -g main.elf >/dev/null || exit 1
     echo "JTAG chain number: " $cable > swm_enable_berts_log.$cable
-    ( echo '1' | ./swm_run.sh $cable | grep 'BERT enable'  >> swm_enable_berts_log.$cable ) &
+    ( echo '1' | nios2-terminal --cable $cable | grep 'BERT enable'  >> swm_enable_berts_log.$cable ) &
     pids="$pids $!"
 done
 wait $pids
